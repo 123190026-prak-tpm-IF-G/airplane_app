c15412a2759c9c8e24f821a56a13cfecf60e9a33
anggitrestuu <123190026@student.upnyk.ac.id>
2023-03-06 22:04:46 +0700

    add assets and slicing get started page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:my_app/ui/pages/get_started_page.dart';
 import 'package:my_app/ui/pages/signin_page.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/ui/pages/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +11,14 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My App',
       initialRoute: '/',
       routes: {
-        '/': (context) => const SignInPage(),
+        '/': (context) => const SplashPage(),
+        '/get-started': (context) => const GetStartedPage(),
+        '/signin': (context) => const SignInPage(),
       },
     );
   }
---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      // User? user = FirebaseAuth.instance.currentUser;
+
+      Navigator.pushNamedAndRemoveUntil(
+          context, '/get-started', (route) => false);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: const EdgeInsets.only(bottom: 50),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icon_plane.png',
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'AIRPLANE',
+              style: whiteTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: medium,
+                letterSpacing: 10,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
